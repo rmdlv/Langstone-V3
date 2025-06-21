2c2bb195533d3b1af9e0d51ca39429fb4598ca62
Vladimir <60815271+rmdlv@users.noreply.github.com>
2025-06-21 19:29:26 +0800

    Update Graphics.h

--- Graphics.h
+++ Graphics.h
@@ -215,8 +215,8 @@ void setPixel(int x, int y, int R, int G, int B)
 {
   int newx;
   int newy;
-  newx = 720 - y;
-  newy = 1280 - x;
+  newx = y;
+  newy = x;
   if (rotatescreen)
   {
     newx = 720 - newx;
